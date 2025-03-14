c150af9cd251bbf617a75d9f3516bca9f8fe152c
axCaffiend <axfulcher@outlook.com>
2025-03-14 05:14:31 +0000

    (script.js): Added autofocus to next/previous fields in user controls

--- script.js
+++ script.js
@@ -82,3 +82,38 @@ userControl.addEventListener("input", (e) => {
             }
         }
     })
+
+    // COPIED CODE - adjusted from:
+    // https://stackoverflow.com/questions/15595652/focus-next-input-once-reaching-maxlength-value
+
+    userControl.addEventListener("keyup", (e) => {
+        let target = e.target;
+        let maxLength = parseInt(target.attributes["maxlength"].value, 10);
+        let myLength = target.value.length;
+        if (myLength >= maxLength) {
+            let next = target;
+            while (next = next.nextElementSibling) {
+                if (next == null)
+                    break;
+                if (next.tagName.toLowerCase() === "input") {
+                    next.focus();
+                    break;
+                }
+            }
+        }
+        // Move to previous field if empty (user pressed backspace)
+        else if (myLength === 0) {
+            let previous = target;
+            while (previous = previous.previousElementSibling) {
+                if (previous == null)
+                    break;
+                if (previous.tagName.toLowerCase() === "input") {
+                    previous.focus();
+                    break;
+                }
+            }
+        }
+    })
+
+
+    
